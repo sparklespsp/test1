20f4b1c42c79204f131f0132f2eb62a1cb91c07b
Liu <3237094548@qq.com>
2024-03-20 22:41:52 +0800

    添加项目文件。

---
+++ test1/test1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+void hanoi(int n, char a, char c, char b) {
+    if (n == 1) {
+        cout << "移动盘 1 由 " << a << " -> " << c << endl;
+    }
+    else {
+        hanoi(n - 1, a, b, c);
+        cout << "移动盘 " << n << " 由 " << a << " -> " << c << endl;
+        hanoi(n - 1, b, c, a);
+    }
+}
+
+int main() {
+    int n;
+    cout << "请输入圆盘的总数量: ";
+    cin >> n;
+    hanoi(n, 'A', 'C', 'B');
+    return 0;
+}
